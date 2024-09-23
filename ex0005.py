37e1275c4418819a011a22d8d8cf8c9955278638
unknown <mateusroberto2609@gmail.com>
2024-09-23 18:32:29 -0300

    Initial commit

---
+++ ex0005.py
@@ -0,0 +1,8 @@
+#Numero antecessor e sucessor
+
+num = int(input("Digite um número "))
+antecessor = num-1
+sucessor = num-2
+
+print('O atencessor de {} é {} '.format(num,antecessor))
+print('O sucessor de {} é {}'.format(num,sucessor))
